5757052f11f9444114962ec6f43a934a33b4ba21
Alexandre Sanches <aleloliv555@hotmail.com>
2024-01-02 19:12:34 -0300

    Adicionados aulas e exercícios sobre enumeracoes e composicao

---
+++ ExercicioEnumEComp/ExercicioEnumEComp/Entities/Order.cs
@@ -0,0 +1,45 @@
+﻿using ExercicioEnumEComp.Entities.Enums;
+
+namespace ExercicioEnumEComp.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() 
+        {
+        }
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        public double Total()
+        {
+            int quantity = 0;
+            double price = 0.00;
+            double sum = 0;
+            foreach (OrderItem item in Items)
+            {
+                quantity = item.Quantity;
+                price = item.Price;
+                sum += (quantity * price);
+            }
+            return sum;
+        }
+        
+    }
+}
---
+++ ExercicioEnumEComp/ExercicioEnumEComp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Globalization;
+using ExercicioEnumEComp.Entities;
+using ExercicioEnumEComp.Entities.Enums;
+
+namespace ExercicioEnumEComp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client's data:");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string clientEmail = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime clientBirthDate = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(clientName, clientEmail, clientBirthDate);
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            string orderStatus = Console.ReadLine();
+            OrderStatus status = Enum.Parse<OrderStatus>(orderStatus);
+
+            DateTime d1 = DateTime.Now;
+
+            Order order = new Order(d1, status, client);
+
+            Console.Write("How many items to this order? ");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1;  i <= num; i++) 
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Product product = new Product(productName, productPrice);
+
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+                OrderItem item = new OrderItem(quantity, productPrice, product);
+
+                order.AddItem(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine("Order moment: " + order.Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            Console.WriteLine("Order status: " + orderStatus);
+            Console.WriteLine(
+                $"Client: " + 
+                client.Name + 
+                " (" + 
+                client.BirthDate.ToString("dd/MM/yyyy") + 
+                ") - " + 
+                client.Email);
+            Console.WriteLine("Order items: ");
+            foreach (OrderItem item in order.Items)
+            {
+                Console.WriteLine(
+                    item.Product.Name + 
+                    ", $" + 
+                    item.Price.ToString("F2") + 
+                    ", Quantity: " + 
+                    item.Quantity + 
+                    ", Subtotal: " + 
+                    item.SubTotal().ToString("F2"), CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine("Total price: $" + order.Total().ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
