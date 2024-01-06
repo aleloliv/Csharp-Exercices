c8e4ba9c14164cc804f03f056e8bdbbe4d0e0064
aleloliv <aleloliv555@hotmail.com>
2024-01-06 19:52:54 -0300

    Jogo de xadrez e aula sobre arquivos adicionado

---
+++ Arquivos/Arquivos/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.IO;
+using System;
+//using System.Collections.Generic;
+
+namespace Arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = @"C:\Users\alelo\OneDrive\Curso\Cursos\C#\file1.txt";
+            //FileStream fs = null;
+            //StreamReader sr = null;
+
+            //try
+            //{
+            //    sr = File.OpenText(path);
+            //    while (!sr.EndOfStream)
+            //    {
+            //        string line = sr.ReadLine();
+            //        Console.WriteLine(line);
+            //    }
+            //}
+            //catch (IOException e)
+            //{
+            //    Console.WriteLine("An error occurred");
+            //    Console.WriteLine(e.Message);
+            //}
+            //finally
+            //{
+            //    if (sr != null) sr.Close();
+            //    if (fs != null) fs.Close();
+            //}
+
+            //string path = @"C:\Users\alelo\OneDrive\Curso\Cursos\C#\file1.txt";
+
+            //try
+            //{
+            //    using (StreamReader sr = File.OpenText(path))
+            //    {
+            //        while (!sr.EndOfStream)
+            //        {
+            //            string line = sr.ReadLine();
+            //            Console.WriteLine(line);
+            //        }
+            //    }
+
+            //}
+            //catch (IOException e)
+            //{
+            //    Console.WriteLine("An error occurred");
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //string sourcePath = @"C:\Users\alelo\OneDrive\Curso\Cursos\C#\file1.txt";
+            //string targetPath = @"C:\Users\alelo\OneDrive\Curso\Cursos\C#\file2.txt";
+
+            //try
+            //{
+            //    string[] lines = File.ReadAllLines(sourcePath);
+
+            //    using (StreamWriter sw = File.AppendText(targetPath))
+            //    {
+            //        foreach (string line in lines)
+            //        {
+            //            sw.WriteLine(line.ToUpper());
+            //        }
+            //    }
+
+            //}
+            //catch (IOException e)
+            //{
+            //    Console.WriteLine("An error occurred");
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //string sourcePath = @"C:\Users\alelo\OneDrive\Curso\Cursos\C#\myfolder";
+
+            //try
+            //{
+            //    /*IEnumerable<string>*/var folders = Directory.EnumerateDirectories(sourcePath, "*.*", SearchOption.AllDirectories);
+            //    Console.WriteLine("FOLDERS: ");
+            //    foreach (string s in folders)
+            //    {
+            //        Console.WriteLine(s);
+            //    }
+            //}
+            //catch (IOException e)
+            //{
+            //    Console.WriteLine("An error occurred");
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //try
+            //{
+            //    /*IEnumerable<string>*/
+            //    var files = Directory.EnumerateFiles(sourcePath, "*.*", SearchOption.AllDirectories);
+            //    Console.WriteLine("FILES: ");
+            //    foreach (string s in files)
+            //    {
+            //        Console.WriteLine(s);
+            //    }
+
+            //    Directory.CreateDirectory(sourcePath + @"\newfolder");
+            //}
+            //catch (IOException e)
+            //{
+            //    Console.WriteLine("An error occurred");
+            //    Console.WriteLine(e.Message);
+            //}
+
+            string path = @"C:\Users\alelo\OneDrive\Curso\Cursos\C#\file1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+        }
+    }
+}
---
+++ ExercicioArquivos/ExercicioArquivos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using ExercicioArquivos.Entities;
+using System.Xml;
+using System.Globalization;
+
+namespace ExercicioArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\alelo\OneDrive\Curso\Cursos\C#\ExercicioArquivos\myfolder\solditems.csv";
+
+            try
+            {
+                List<Product> products = new List<Product>();
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] splitlines = line.Split(',');
+                        Product product = new Product(splitlines[0], double.Parse(splitlines[1], CultureInfo.InvariantCulture), int.Parse(splitlines[2]));
+                        products.Add(product);
+                    }
+                }
+
+                Directory.CreateDirectory(@"C:\Users\alelo\OneDrive\Curso\Cursos\C#\ExercicioArquivos\myfolder\out");
+                string outputPath = @"C:\Users\alelo\OneDrive\Curso\Cursos\C#\ExercicioArquivos\myfolder\out\summary.csv";
+
+                using (StreamWriter sw = File.AppendText(outputPath))
+                {
+                    foreach (Product product1 in products)
+                    {
+                        sw.WriteLine(product1);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
