bc3b047f51231c1cc1afe6411f66cebb917bf7c6
aleloliv <aleloliv555@hotmail.com>
2024-01-27 14:36:01 -0300

    Fixation exercise

---
+++ ExercicioFixacaoInterface/ExercicioFixacaoInterface/Services/IOnlinePaymentService.cs
@@ -0,0 +1,8 @@
+﻿namespace ExercicioFixacaoInterface.Services
+{
+    internal interface IOnlinePaymentService
+    {
+        double Interest(double amount, int months);
+        double PaymentFee(double amount);
+    }
+}
---
+++ ExercicioFixacaoInterface/ExercicioFixacaoInterface/Services/PayPalService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioFixacaoInterface.Services
+{
+    internal class PayPalService : IOnlinePaymentService
+    {
+        private const double FeePercentage = 0.02;
+        private const double MonthlyInterest = 0.01;
+
+        public double Interest(double amount, int months)
+        {
+            return amount * MonthlyInterest * months;
+        }
+
+        public double PaymentFee(double amount)
+        {
+            return amount * FeePercentage;
+        }
+    }
+}
