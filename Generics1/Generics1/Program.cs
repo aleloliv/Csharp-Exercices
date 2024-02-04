41b98f26fac73bd0769bc76895d8a4ac418931fe
aleloliv <aleloliv555@hotmail.com>
2024-02-03 22:21:56 -0300

    Generics and it's restrictions

---
+++ Generics1/Generics1/Program.cs
@@ -0,0 +1,30 @@
+﻿using Generics1.Services;
+using System;
+using System.Collections.Generic;
+
+namespace Generics1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int num = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < num; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            int a = printService.First();
+            int b = a + 2;
+            Console.WriteLine(b);
+
+            printService.Print();
+            Console.WriteLine("First: " +  printService.First());
+        }
+    }
+}
